c3d9f6bfa1aa4c20b318aeab9e647cef78b432b9
mariarf <mariarf1478@gmail.com>
2021-03-14 00:38:06 +0000

    airquality con link de juanma

--- airQualityApi.py
+++ airQualityApi.py
@@ -2,21 +2,22 @@ import requests
 import json
 
 #if _name_ =='_airQualityApi_':
-url= 'https://aqicn.org/snapshot/usa/newyork/20210313-16/?cs'
+url= 'https://api.waqi.info/feed/newyork/?token=c889c21d8944202ace2d717bb97db9ab84160449'
 response=requests.get(url)
 print(response)
 if response.status_code == 200:
 
 
     #colocando en json para acceder a una variable con json
-    """
+    
     response_json = json.loads(response.text)
     city = response_json["data"]["city"]["name"]
     print(city)
+    
     #se concadenan las variables y [2] seria entrando al 3er parametro de o3
-    data = response_json["data"]["forecast"]["daily"]["o3"][2]
+    data = response_json["data"]["forecast"]["daily"]["o3"][0]
     print(data)
-    """
+    
 """
     #colocando en json para acceder a una variable con request
     response_json = response.json()  #esto es un diccionario
