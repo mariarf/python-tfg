0e5766b2d2925d410c69b008d13607dca93eaf09
mariarf <mariarf1478@gmail.com>
2021-04-01 17:01:16 +0100

    detalles demain y traffic formato nuevo

--- apis/mainApis.py
+++ apis/mainApis.py
@@ -1,25 +1,69 @@
+from numpy import column_stack
 from trafficApi import *
 from airQualityApi import *
 from weatherApi import *
 import os
 import pandas as pd
 
+def apisRequest():
+    data_folder = os.getcwd().split("\TFG")[0] + "/TFG/apis_data"
+    file_to_open = data_folder + "/traffic_dataIngestion.csv"
 
-data_folder = os.getcwd().split("\TFG")[0] + "/TFG/apis_data"
-file_to_open = data_folder + "/traffic_dataIngestion.csv"
+    traffic_df = pd.read_csv(file_to_open)
+    last_traffic_date = traffic_df.loc[traffic_df.index[-1], "date"]
+    last_traffic_time = traffic_df.loc[traffic_df.index[-1], "time"]
 
-traffic_df = pd.read_csv(file_to_open)
-last_traffic_date = traffic_df.loc[traffic_df.index[-1], "date"]
-last_traffic_time = traffic_df.loc[traffic_df.index[-1], "time"]
+    start_datetime = last_traffic_date + "T" + last_traffic_time
+    #start_datetime = "2021-03-27T23:59:04"
+    trafficDataIngestion(1000000, start_datetime)
 
-start_datetime = last_traffic_date + "T" + last_traffic_time
-start_datetime = "2021-03-01T00:00:00"
-trafficDataIngestion(5000, start_datetime)
+    traffic_df = pd.read_csv(file_to_open)
+    last_traffic_date = traffic_df.loc[traffic_df.index[-1], "date"]
+    last_traffic_time = traffic_df.loc[traffic_df.index[-1], "time"]
 
-traffic_df = pd.read_csv(file_to_open)
-last_traffic_date = traffic_df.loc[traffic_df.index[-1], "date"]
-last_traffic_time = traffic_df.loc[traffic_df.index[-1], "time"]
+    end_datetime = last_traffic_date + "T" + last_traffic_time
+    airQualityDataIngestion(start_datetime, end_datetime)
+    weatherDataIngestion(start_datetime, end_datetime)
 
-end_datetime = last_traffic_date + "T" + last_traffic_time
-airQualityDataIngestion(start_datetime, end_datetime)
-weatherDataIngestion(start_datetime, end_datetime)
+
+
+def totalDate():
+
+    data_folder = os.getcwd().split("\TFG")[0] + "/TFG/apis_data/historical"
+    traffic_file = data_folder + "/traffic_historical.csv"
+    airQuality_file = data_folder + "/airQuality_historical.csv"
+    weather_file = data_folder + "/weather_historical.csv"
+
+    traffic_df = pd.read_csv(traffic_file)
+    airQuality_df = pd.read_csv(airQuality_file)
+    weather_df = pd.read_csv(weather_file)
+    
+    airQuality_df["datetime"] = airQuality_df["date"] + "T" + airQuality_df["time"]
+    weather_df["datetime"] = weather_df["date"] + "T" + weather_df["time"]
+    traffic_df["datetime"] = traffic_df["date"] + "T" + traffic_df["time_hour"]
+
+
+    airQuality_df["datetime"] = pd.to_datetime(airQuality_df["datetime"])
+    weather_df["datetime"] = pd.to_datetime(weather_df["datetime"])
+    traffic_df["datetime"] = pd.to_datetime(traffic_df["datetime"])
+
+    print(traffic_df.head())
+    print(airQuality_df.head())
+    print(weather_df.head())
+
+    df = pd.merge(traffic_df,airQuality_df, how= 'inner', on = 'datetime')
+    df = pd.merge(df, weather_df, how="inner", on="datetime")
+    print(df.dtypes)
+
+    df = df[["date_x", "date_y", "date", "time_x", "time_y", "time", "weekday", "id", "speed", "travel_time", "link_name", "aqi", "Dew Point", "Relative Humidity", "Wind Speed", "Wind Gust", "Wind Direction", "Precipitation", "Snow Depth", "Visibility", "Cloud Cover", "Sea Level Pressure", "Conditions"]]
+
+
+
+
+    file_name = data_folder + "/total_historical_march01_to_march27.csv"
+
+    df.to_csv(file_name,index=False)
+
+
+#apisRequest()
+totalDate()
--- apis/trafficApi.py
+++ apis/trafficApi.py
@@ -9,7 +9,7 @@ from sodapy import Socrata
 import os, datetime
 
 #Date tiene que ser de formato: yyyy-mm-ddThh:mm:ss
-def trafficDataIngestion(dataLimit, date):
+def trafficDataIngestion(dataLimit, start_datetime, end_datetime):
 
     # Unauthenticated client only works with public data sets. Note 'None'
     # in place of application token, and no username or password:
@@ -17,25 +17,35 @@ def trafficDataIngestion(dataLimit, date):
 
     # First dataLimit results, returned as JSON from API / converted to Python list of
     # dictionaries by sodapy.
-    date = "data_as_of >" + "'" + date + "'" 
-    results = client.get("i4gi-tjb9", limit=dataLimit, borough = "Manhattan", where = date)
+    #date = "data_as_of >" + "'" + date + "'"  #para a partir de una fecha
+    date = f"data_as_of between '{start_datetime}' and '{end_datetime}'"
+    
+    #results = client.get("i4gi-tjb9", limit=dataLimit, borough = "Manhattan", where = date) #para a partir de una fecha
+    results = client.get("i4gi-tjb9", borough = "Manhattan", where = date)
+    
     
     # Convert to pandas DataFrame
     results_df = pd.DataFrame.from_records(results)
-    
-    #tipografia de los datos, separando datos de fecha en fecha y hora     ---------------------------------------
-    results_df["time"] = results_df["data_as_of"].str.split("T").str.get(1) 
-    results_df["time"] = results_df["time"].str.replace(".000", "")
-    results_df["time_hour"] = results_df["time"].str.split(":").str.get(0) + ":00:00"
-    results_df["time"] = pd.to_datetime(results_df["time"])
-    results_df["date"] = results_df["data_as_of"].str.split("T").str.get(0) 
+   
+    #-----------------------------------------datetime - time_hour --------------------------------------#
+    results_df["time"] = results_df["data_as_of"].str[11:19]
+    results_df["date"] = results_df["data_as_of"].str[0:11]
+    results_df["time_hour"] = results_df["time"].str[0:3] + "00:00"
+
+    results_df["datetime"] = results_df["date"] + results_df["time_hour"]
+    results_df["datetime_traffic"] = results_df["date"] + results_df["time"]
+
     results_df["date"]=pd.to_datetime(results_df["date"])
     results_df["weekday"] = results_df['date'].dt.day_name()
-
    
+    results_df["datetime"] = pd.to_datetime(results_df["datetime"])
+    results_df["datetime_traffic"] = pd.to_datetime(results_df["datetime_traffic"])
+
+
     #filtrando datos ---------------------------------------------------------------------------
-    traffic = results_df[["id", "speed", "travel_time", "date", "time","borough", "link_name", "weekday", "time_hour"]]
-    
+    traffic = results_df[["datetime", "datetime_traffic", "weekday", "id", "speed", "travel_time", "link_name"]]
+    print(traffic.head(15))
+   
     #guardando -----------------------------------------------------------------------------------------
     current_dir = os.getcwd().split("\TFG")[0] 
     filename = current_dir + "/TFG/apis_data/traffic_dataIngestion.csv"
@@ -45,6 +55,6 @@ def trafficDataIngestion(dataLimit, date):
 
 
     
-#lastRegister = trafficDataIngestion(10, "2021-03-21T01:00:00.000")
-#lastRegister = trafficDataIngestion(10, "2021-03-22T02:00:00.000")
+trafficDataIngestion(1000000, "2021-03-01T15:00:00.000", "2021-03-01T23:00:00.000")
+#trafficDataIngestion(10, "2021-03-22T02:00:00.000")
 
