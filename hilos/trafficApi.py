cec6367e04afee6187c58a23cdcc01027535f64b
mariarf <mariarf1478@gmail.com>
2021-04-19 00:20:40 +0100

    mejorando codigo

--- hilos/trafficApi.py
+++ hilos/trafficApi.py
@@ -2,56 +2,48 @@
 # pip install pandas
 # pip install sodapy
 
-from numpy import select
+from types import TracebackType
 import pandas as pd
 from sodapy import Socrata
-import os
+import traceback
+import sys
 
-#Date tiene que ser de formato: yyyy-mm-ddThh:mm:ss
-def trafficDataIngestion(datalimit, start_datetime, end_datetime, file_dir):
+#datetime format: yyyy-mm-ddThh:mm:ss
+#devuelve false si el dataframe esta vacio y true si se escribe algo
+def trafficDataIngestion(datalimit, start_datetime, file_dir):
 
     # Unauthenticated client only works with public data sets. Note 'None'
     # in place of application token, and no username or password:
     client = Socrata("data.cityofnewyork.us", None)
 
-    # First dataLimit results, returned as JSON from API / converted to Python list of
-    # dictionaries by sodapy.
-    date = f"data_as_of >'{start_datetime}'"  #para a partir de una fecha
-    #date = f"data_as_of between '{start_datetime}' and '{end_datetime}'"
+    date = f"data_as_of >'{start_datetime}'"  #se define la fecha de inicio de la consulta
     
-    columns = "data_as_of, id, speed, travel_time, link_name"
+    columns = "data_as_of, id, speed, travel_time, link_name"  #columnas que se pediran a la api
 
-    results = client.get("i4gi-tjb9", limit=datalimit, borough = "Manhattan", where = date) #para a partir de una fecha
-    #results = client.get("i4gi-tjb9", limit = datalimit, borough = "Manhattan", where = date, select = columns)
+    results = client.get("i4gi-tjb9", limit=datalimit, borough = "Manhattan", where = date, select = columns) 
     
     # Convert to pandas DataFrame
     results_df = pd.DataFrame.from_records(results)
-   
+    if results_df.empty:
+        return False
     #-----------------------------------------datetime - time_hour --------------------------------------#
-    results_df["time"] = results_df["data_as_of"].str[11:19]
-    results_df["date"] = results_df["data_as_of"].str[0:11]
-    results_df["time_hour"] = results_df["time"].str[0:3] + "00:00"
-
-    results_df["datetime"] = results_df["date"] + results_df["time_hour"]
-    results_df["datetime_traffic"] = results_df["date"] + results_df["time"]
-
-    results_df["date"]=pd.to_datetime(results_df["date"])
-    results_df["weekday"] = results_df['date'].dt.day_name()
-   
+    results_df["datetime"] = results_df["data_as_of"].str[:-9] + "00:00"
+    results_df["datetime_traffic"] = results_df["data_as_of"].str[:-4]
+    
     results_df["datetime"] = pd.to_datetime(results_df["datetime"])
-    results_df["datetime_traffic"] = pd.to_datetime(results_df["datetime_traffic"])
+    results_df["weekday"] = results_df['datetime'].dt.day_name()
+    results_df["datetime"] = results_df["datetime"].dt.strftime("%Y-%m-%dT%H:%M:%S")
 
+    # se añaden nuevas columnas
     results_df = results_df[["datetime", "datetime_traffic", "weekday", "id", "speed", "travel_time", "link_name"]]
-   
-    #guardando -----------------------------------------------------------------------------------------
-    #current_dir = os.getcwd().split("\TFG")[0] 
-    #file_name = current_dir + f"/TFG/apis_data/traffic_historical/traffic_dataIngestion_{start_datetime[0:13]}_to_{end_datetime[0:13]}.csv"
 
+    #se guarda en la direccion del archivo pasado como parametro
     results_df.to_csv(file_dir, index=False)
     print(f"TrafficApi: {file_dir}")
+    return True
 
 
-trafficDataIngestion(10, "2021-02-01T00:00:00", "2021-02-28T23:59:59", "borrame.csv")
+#print(trafficDataIngestion(100000, "2021-04-18T14:00:00", "borrame.csv"))
 #trafficDataIngestion(1000000, "2021-03-01T00:00:00", "2021-03-31T23:59:59")
 
 
