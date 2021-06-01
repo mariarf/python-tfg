b1087b5bbc10f625be9c16113ff192138bd7e4db
mariarf <mariarf1478@gmail.com>
2021-06-01 19:04:15 +0100

    comentando el codigo que no se usa

--- historical_apis/mainHistoricalApis.py
+++ historical_apis/mainHistoricalApis.py
@@ -8,19 +8,19 @@ from datetime import datetime as dt
 
 
 def apisRequest():
-    data_folder = os.getcwd().split("\TFG")[0] + "/TFG/apis_data"
-    file_to_open = data_folder + "/historical_register.csv"
+    #data_folder = os.getcwd().split("\TFG")[0] + "/TFG/apis_data/2019"
+    #file_to_open = data_folder + "/historical_register.csv"
 
-    datetime_df = pd.read_csv(file_to_open)
-    start_datetime = datetime_df.loc[datetime_df.index[-1], "datetime"]
-    #start_datetime = "2021-03-27T23:59:04"
+    #datetime_df = pd.read_csv(file_to_open)
+    #start_datetime = datetime_df.loc[datetime_df.index[-1], "datetime"]
+    #start_datetime = "2019-01-01T00:00:01"
 
-    tz_NY = pytz.timezone('America/New_York') 
-    end_datetime = datetime.datetime.now(tz_NY)
-    end_datetime = end_datetime - datetime.timedelta(hours=4)
-    end_datetime = end_datetime.strftime("%Y-%m-%dT%H:%M:%S")
+    #tz_NY = pytz.timezone('America/New_York') 
+    #end_datetime = datetime.datetime.now(tz_NY)
+    #end_datetime = end_datetime - datetime.timedelta(hours=4)
+    #end_datetime = end_datetime.strftime("%Y-%m-%dT%H:%M:%S")
 
-    start_datetime = datetime.datetime.strptime("2021-01-01T00:00:00","%Y-%m-%dT%H:%M:%S")
+    start_datetime = datetime.datetime.strptime("2020-01-01T00:00:00","%Y-%m-%dT%H:%M:%S")
     end_datetime = start_datetime + datetime.timedelta(days=14, hours= 23, seconds=3599)
     end_datetime = str(end_datetime).replace(" ","T")
     start_datetime = str(start_datetime).replace(" ","T")
@@ -29,7 +29,7 @@ def apisRequest():
     #2020-febrero
     #enero, marzo, mayo, julio, agosto, octubre, diciembre,
     count = 0
-    while count < 7:  
+    while count < 25:  
 
     
         trafficDataIngestion(10000000, start_datetime, end_datetime)
@@ -65,7 +65,7 @@ def apisRequest():
 
 def mergeByDatetime(datetime):
 
-    data_folder = os.getcwd().split("\TFG")[0] + "/TFG/apis_data/"
+    data_folder = os.getcwd().split("\TFG")[0] + "/TFG/apis_data"
 
     traffic_file = data_folder + f"/traffic_historical/traffic_dataIngestion_{datetime}.csv"
     airQuality_file = data_folder + f"/airQuality_historical/airQuality_dataIngestion_{datetime}.csv"
@@ -85,7 +85,7 @@ def mergeByDatetime(datetime):
     
     df = df.sort_values("datetime")
 
-    file_name = data_folder + f"/merge_{datetime}.csv"
+    file_name = data_folder + f"/2020/merge_{datetime}.csv"
 
     df.to_csv(file_name,index=False)
 
